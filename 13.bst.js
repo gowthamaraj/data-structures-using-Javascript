b94a06f351969a90042e15b8c4df3c824b19a4d3
gowthamaraj <gowthamaraj360@gmail.com>
2020-04-17 00:06:49 +0530

    bst added/mod

--- 13.bst.js
+++ 13.bst.js
@@ -1,71 +1,54 @@
-const LEFT = 0
-const RIGHT = 1
-
-class TreeNode{
-    constructor(value){
-        this.value = value
-        this.descendents =[]
-        this.parent = null
-    }
-
-    get left(){
-        return this.descendents[LEFT]
-    }
-
-    set left(node){
-        this.descendents[LEFT] = node
-        if(node){
-            node.parent = this
-        }
-    }
-    get right(){
-        return this.descendents[RIGHT]
-    }
-
-    set right(node){
-        this.descendents[RIGHT] = node
-        if(node){
-            node.parent = this
-        }
+class Node{
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
     }
 }
 
-class BinarySearchTree {
+class tree{
     constructor(){
-        this.root = null;
-        this.size= 0
+        this.root = null 
     }
 
-    add(value){
-        const newNode = new TreeNode(value)
-        if(this.root){
-            const { found, parent } = this.findNodeAndParent(value);
-            if(value < parent.value){
-                parent.LEFT = newNode
-            }else{
-                parent.RIGHT = newNode
-            }
+    // add
+   add(val){
+       if(this.root == null){
+           this.root = new Node(val)
+       }
+       else{
+            let node = this.root
+            this.addHelper(node,val)
+       }
+   }
+   addHelper(node,val){
+    if(val>node.val){
+         if(node.right == null){
+             node.right = new Node(val)
+         }else{
+            this.addHelper(node.right,val)
+         }
+    }else{
+        if(node.left == null){
+            node.left = new Node(val)
+        }else{
+           this.addHelper(node.left,val)
         }
-        else{
-            this.root = newNode
-        }
-        this.size += 1;
-        return newNode;
     }
-    findNodeAndParent(value){
-        let node = this.root
-        let parent
-        while(node){
-            if(node.value === value){
-                break
-            }
-            parent = node
-            node=(value>=node.value)?node.right:node.left
-        }
-        return {found: node, parent}
-    }
-    find(value)
-    remove(value)
-    getMax()
-    getMin()
+   }
+
+
+//    traverse
+   traverse(){
+       let node = this.root
+       
+   }
 }
+
+let t = new tree()
+t.add(1)
+console.log(t);
+t.add(2)
+console.log(t);
+t.add(3)
+console.log(t);

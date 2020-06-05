fcb4fd936d9db0ffbd656ecc433a811f230779f6
gowthamaraj <gowthamaraj360@gmail.com>
2020-06-06 00:08:59 +0530

    feat: factorial and fibonacci in recursion.js

--- 1.recursion.js
+++ 1.recursion.js
@@ -6,6 +6,17 @@ function fibonacci(n){
     return fibonacci(n-2) + fibonacci(n-1)
 }
 
+const factorial = (n) => {
+    if(n<2){
+        return 1
+    }
+    //base case
+    else{
+        return factorial(n-1)*n
+    }
+}
+
 for(let i=1;i<20;i++){
     console.log(fibonacci(i))
+    console.log(factorial(i))
 }
